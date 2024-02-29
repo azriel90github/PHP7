a455d4bd85d43aea4c6224d87026d97f5f3cc35d
azriel90github <azrielgithub@gmail.com>
2024-02-29 18:04:12 +0000

    variaveis

---
+++ ex002-Variaveis/index.php
@@ -0,0 +1,11 @@
+
+<?php 
+
+$nome = "Azriel Armando,"; //variavel com a string azriel armando
+
+echo $nome; //mostrar na tela a variavel $nome 
+
+
+var_dump($nome) // informações extras sobre a variavel
+
+?>
